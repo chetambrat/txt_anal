a88b7323570093c7e9d4e73a0dd518d49afa9e6f
chetambrat <chetambrat@users.noreply.github.com>
2020-04-13 21:35:03 +0300

    added hierarch

--- task2/task2.py
+++ task2/task2.py
@@ -1,4 +1,10 @@
-import gensim
+from sklearn.cluster import AgglomerativeClustering
+from scipy.cluster.hierarchy import linkage, dendrogram
+from itertools import combinations
+import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def memoize(func):
@@ -43,9 +49,10 @@ def hamming(first_word, second_word):
 def jaccard(first_word, second_word):
     word1 = set(first_word)
     word2 = set(second_word)
-    return float(len(word1.intersection(word2))) / len(word1.union(word2))
+    return 1 - float(len(word1.intersection(word2))) / len(word1.union(word2))
 
 
+@memoize
 def dam_lev_d(first_word, second_word):
     """ ref: wiki """
     d = {}
@@ -75,6 +82,7 @@ def dam_lev_d(first_word, second_word):
 
 
 class Words:
+    """ API for word testing """
     def __init__(self, first_word, second_word):
         self.first_word = first_word
         self.second_word = second_word
@@ -84,13 +92,58 @@ class Words:
             f"for words {self.first_word, self.second_word} \n hamming distance is: {hamming(self.first_word, self.second_word)} "
             f"\n levenstein distance is: {levenshtein(self.first_word, self.second_word)} \n jaccard metric is:"
             f" {jaccard(self.first_word, self.second_word)} "
-            f"\n dam lev distance is"
+            f"\n dam lev distance is:"
             f" {dam_lev_d(self.first_word, self.second_word)}")
 
 
-example1 = Words("abc", "dbc")
-example2 = Words("abc", "a")
+def initialize_output(*args):
+    instance = Words(*args)
+    return instance.result()
 
 
-example1.result()
-example2.result()
+initialize_output("abc", "dbc")
+initialize_output("abc", "a")
+
+
+def process_file(path, method):
+    with open(path, 'r', encoding="utf8") as f:
+        input_file = f.read()
+        splitted = re.split("[.? ,()\"—“”…]", input_file)
+        output = [elt for elt in splitted if elt != ""]
+        external_array = []
+        for elt1 in output:
+            internal_array = []
+            for elt2 in output:
+                internal_array.append(method(elt1, elt2))
+            external_array.append(internal_array)
+        return external_array
+
+
+def plot_dendrogram(cluster_model, **kwargs):
+    """ ref: from scikit website """
+
+    # Children of hierarchical clustering
+    children = cluster_model.children_
+
+    # Distances between each pair of children
+    # Since we don't have this information, we can use a uniform one for plotting
+    distance = np.arange(children.shape[0])
+
+    # The number of observations contained in each cluster level
+    no_of_observations = np.arange(2, children.shape[0]+2)
+
+    # Create linkage matrix and then plot the dendrogram
+    linkage_matrix = np.column_stack([children, distance, no_of_observations]).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+
+output = process_file('./text_file.txt', hamming)
+x = np.array(output, np.int32)
+model = AgglomerativeClustering(n_clusters=2, affinity="precomputed", memory="./.cache", linkage="average")
+
+model = model.fit(x)
+plt.title('hamming')
+plot_dendrogram(model, labels=model.labels_)
+plt.show()
